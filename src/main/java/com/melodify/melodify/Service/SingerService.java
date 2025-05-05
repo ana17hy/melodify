bc350314a32011da6db2f81ba78bc9cbbfbdb7a8
ana17hy <ana.huapaya@utec.edu.pe>
2025-05-05 08:07:30 -0500

    Actualizadoo

--- src/main/java/com/melodify/melodify/Service/SingerService.java
+++ src/main/java/com/melodify/melodify/Service/SingerService.java
@@ -2,29 +2,32 @@ package com.melodify.melodify.Service;
 
 import com.melodify.melodify.Entity.Singer; // usamos la clase singer
 import com.melodify.melodify.Repository.SingerRepository; // importamos el repositorio
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.List; // para la lista
+import java.util.Optional; // para el Optional -> puede que exista o que no exista
 
 @Service
-public class SingerService {
-    @Autowired
-    private SingerRepository singerRepository;
+public class SingerService { // creamos la clase
+    @Autowired // conectar con el repositorio automaticamente
+    private SingerRepository singerRepository; // creamos variable para guardar, buscar, eliminar, etc -> lo hace el repository
 
     public List<Singer> getAllSingers() {
         return singerRepository.findAll();
     }
-
+    // List<>getAllSinger -> pide al repositorio que busque la lista de cantantes
     public Optional<Singer> getSingerById(Long id) {
+    // Optional<>getSingerById -> busca un cantante por su Id, capaz no lo encuentra y devuelve vacio D:
         return singerRepository.findById(id);
     }
 
     public Singer createSinger(Singer singer) {
         return singerRepository.save(singer);
     }
+    // recibe un cantante, lo guarda en el repositorio, te devuelve el cantante guardado
     public void deleteSinger(Long id) {
         singerRepository.deleteById(id);
     }
+    // borra el cantante, lo elimina del repositorio y no devuelve nada -> void
 }
